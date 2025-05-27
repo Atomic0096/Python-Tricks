485dd0adad3d2298bbafd80c01ecef614a3260ee
Suica <104819634+Atomic0096@users.noreply.github.com>
2025-05-27 08:45:13 +0000

    使用requests下载文件

---
+++ download.py
@@ -0,0 +1,12 @@
+import requests
+
+def download(url, filename):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(filename, "wb") as f:
+            f.write(response.content)
+
+path = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/us-states.json'
+download(path, "us-states.json")
+
+filename = "us-states.json"
