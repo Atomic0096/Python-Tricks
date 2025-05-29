d0792e02690801beead3382fe095ca01d4db9b84
Suica <104819634+Atomic0096@users.noreply.github.com>
2025-05-29 07:44:24 +0000

    更新了download函数

--- download.py
+++ download.py
@@ -1,12 +1,13 @@
+import os
 import requests
 
-def download(url, filename):
+url_test = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0101EN-SkillsNetwork/labs/Module%205/data/Example1.txt"
+path_test = os.path.join(os.getcwd(),"example.txt")
+
+def download(url, path):
     response = requests.get(url)
     if response.status_code == 200:
-        with open(filename, "wb") as f:
+        with open(path, "wb") as f:
             f.write(response.content)
 
-path = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/us-states.json'
-download(path, "us-states.json")
-
-filename = "us-states.json"
+download(url_test, path_test)
