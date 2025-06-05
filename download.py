bd9bd3b1a7668bf9124e0e823c96bb09a8ecd8fa
Suica <104819634+Atomic0096@users.noreply.github.com>
2025-06-05 18:06:25 +0800

    Add files via upload

---
+++ download.py
@@ -0,0 +1,13 @@
+import os
+import requests
+
+url_test = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0101EN-SkillsNetwork/labs/Module%205/data/Example1.txt"
+path_test = os.path.join(os.getcwd(),"example.txt")
+
+def download(url, path):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(path, "wb") as f:
+            f.write(response.content)
+
+download(url_test, path_test)
